16169450aaf9fba2ca08c4c103154b1125be9da4
Cackerman55 <cackerman55@gmail.com>
2016-01-18 10:56:13 -0500

    Added speedcontrollers and drivetrain to RobotMap

--- src/org/usfirst/frc/team5590/robot/RobotMap.java
+++ src/org/usfirst/frc/team5590/robot/RobotMap.java
@@ -5,14 +5,40 @@ package org.usfirst.frc.team5590.robot;
  * the wiring easier and significantly reduces the number of magic numbers
  * floating around.
  */
+import edu.wpi.first.wpilibj.*;
+import edu.wpi.first.wpilibj.livewindow.LiveWindow;
+import edu.wpi.first.wpilibj.livewindow.LiveWindowSendable;
+
+import java.util.Vector;
+import java.util.concurrent.Executor;
+import java.util.concurrent.ExecutorService;
+
 public class RobotMap {
-    // For example to map the left and right motors, you could define the
-    // following variables to use with your drivetrain subsystem.
-    // public static int leftMotor = 1;
-    // public static int rightMotor = 2;
+	public static SpeedController drivetrainLeftTrack;
+    public static SpeedController drivetrainRightTrack;
+    public static RobotDrive robotDrive;
+    public static SpeedController ballCollector;
+    public static SpeedController ballShooter;
     
-    // If you are using multiple modules, make sure to define both the port
-    // number and the module. For example you with a rangefinder:
-    // public static int rangefinderPort = 1;
-    // public static int rangefinderModule = 1;
+    public static void init(){
+    	drivetrainLeftTrack = new TalonSRX(1);
+    	LiveWindow.addActuator("Drivetrain", "Left Track", (LiveWindowSendable) drivetrainLeftTrack);
+    	
+    	drivetrainRightTrack = new TalonSRX(2);
+    	LiveWindow.addActuator("Drivetrain", "Left Track", (LiveWindowSendable) drivetrainRightTrack);
+    
+    	robotDrive = new RobotDrive(drivetrainLeftTrack, drivetrainRightTrack);
+    	
+    	robotDrive.setSafetyEnabled(false);
+    	robotDrive.setExpiration(.1);
+    	robotDrive.setSensitivity(.5);
+    	robotDrive.setMaxOutput(1.0);
+    
+    	ballCollector = new TalonSRX(3);
+    	LiveWindow.addActuator("Ball Collector", "Collector", (LiveWindowSendable) ballCollector);
+    
+    	ballShooter = new TalonSRX(4);
+    	LiveWindow.addActuator("Ball Shooter", "Shooter", (LiveWindowSendable) ballShooter);
+    
+    }
 }
