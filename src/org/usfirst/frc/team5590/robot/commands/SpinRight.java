92a3aa1618b729d35f5639282ecb7e3a17bd062d
Cross Cordosi <ccordosi1@icloud.com>
2016-01-18 13:03:14 -0500

    Added Stop Command Class

--- src/org/usfirst/frc/team5590/robot/commands/SpinRight.java
+++ src/org/usfirst/frc/team5590/robot/commands/SpinRight.java
@@ -1,19 +1,26 @@
 package org.usfirst.frc.team5590.robot.commands;
 
+import org.usfirst.frc.team5590.robot.Robot;
+
 import edu.wpi.first.wpilibj.command.Command;
 
-/**
- *
+/**This command sets the left track motor speed to 0.3 
+ * and right track motor speed to -0.3
+ *To spin the robot right
  */
 public class SpinRight extends Command {
 
     public SpinRight() {
         // Use requires() here to declare subsystem dependencies
         // eg. requires(chassis);
+    	
+    	requires (Robot.drivetrain);
     }
 
     // Called just before this Command runs the first time
     protected void initialize() {
+    	Robot.drivetrain.stop();
+    	Robot.drivetrain.rotateRight(0.3);
     }
 
     // Called repeatedly when this Command is scheduled to run
@@ -27,6 +34,8 @@ public class SpinRight extends Command {
 
     // Called once after isFinished returns true
     protected void end() {
+    	Robot.drivetrain.stop();
+    	
     }
 
     // Called when another command which requires one or more of the same
--- src/org/usfirst/frc/team5590/robot/commands/Stop.java
+++ src/org/usfirst/frc/team5590/robot/commands/Stop.java
@@ -4,22 +4,19 @@ import org.usfirst.frc.team5590.robot.Robot;
 
 import edu.wpi.first.wpilibj.command.Command;
 
-/**This command sets the left track motor speed to -0.3
- *and the right track motor speed to 0.0
- *to spin the robot left
+/** Stops The Robot by setting both track motor speeds to 0
+ *
  */
-public class SpinLeft extends Command {
+public class Stop extends Command {
 
-    public SpinLeft() {
+    public Stop() {
         // Use requires() here to declare subsystem dependencies
-    	
-    	requires(Robot.drivetrain);
+        requires(Robot.drivetrain);
     }
 
     // Called just before this Command runs the first time
     protected void initialize() {
     	Robot.drivetrain.stop();
-    	Robot.drivetrain.rotateLeft(0.3);
     }
 
     // Called repeatedly when this Command is scheduled to run
@@ -33,11 +30,11 @@ public class SpinLeft extends Command {
 
     // Called once after isFinished returns true
     protected void end() {
-    	Robot.drivetrain.stop();
     }
 
     // Called when another command which requires one or more of the same
     // subsystems is scheduled to run
     protected void interrupted() {
+    	Robot.drivetrain.stop();
     }
 }
