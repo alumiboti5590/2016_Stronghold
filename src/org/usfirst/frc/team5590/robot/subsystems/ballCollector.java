224d2b73e12f3ba8fc62ea05afbc8c5dcdbc4bfa
Cackerman55 <cackerman55@gmail.com>
2016-01-18 12:35:31 -0500

    Created ballCollector subsystem class and added functions

---
+++ src/org/usfirst/frc/team5590/robot/subsystems/ballCollector.java
@@ -0,0 +1,28 @@
+package org.usfirst.frc.team5590.robot.subsystems;
+
+import edu.wpi.first.wpilibj.command.Subsystem;
+
+import edu.wpi.first.wpilibj.*;
+import edu.wpi.first.wpilibj.command.Subsystem;
+import org.usfirst.frc.team5590.robot.RobotMap;
+
+/**
+ *
+ */
+public class ballCollector extends Subsystem {
+    
+    SpeedController ballCollector = RobotMap.ballCollectorMotor;
+	
+    public void initDefaultCommand() {
+       
+    }
+    
+    public void collect(){
+    	ballCollector.set(.3);
+    }
+    
+    public void stop(){
+    	ballCollector.set(0.0);
+    }
+}
+
