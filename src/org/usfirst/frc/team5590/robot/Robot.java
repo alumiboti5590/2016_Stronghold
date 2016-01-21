55b338792c0457513a3a320286b6b5f200026eb9
Cross Cordosi <ccordosi1@icloud.com>
2016-01-21 18:13:56 -0500

    Cleaned up Code

--- src/org/usfirst/frc/team5590/robot/Robot.java
+++ src/org/usfirst/frc/team5590/robot/Robot.java
@@ -1,17 +1,14 @@
 
 package org.usfirst.frc.team5590.robot;
 
-import org.usfirst.frc.team5590.robot.commands.ExampleCommand;
-import org.usfirst.frc.team5590.robot.subsystems.Drivetrain;
 import org.usfirst.frc.team5590.robot.subsystems.BallCollector;
-import org.usfirst.frc.team5590.robot.subsystems.ExampleSubsystem;
+import org.usfirst.frc.team5590.robot.subsystems.Drivetrain;
 
 import edu.wpi.first.wpilibj.IterativeRobot;
 import edu.wpi.first.wpilibj.command.Command;
 import edu.wpi.first.wpilibj.command.Scheduler;
 import edu.wpi.first.wpilibj.livewindow.LiveWindow;
 import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
-import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
 /**
  * The VM is configured to automatically run this class, and to call the
@@ -22,7 +19,6 @@ import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
  */
 public class Robot extends IterativeRobot {
 
-	public static final ExampleSubsystem exampleSubsystem = new ExampleSubsystem();
 	public static final Drivetrain drivetrain = new Drivetrain();
 	public static final BallCollector ballCollector = new BallCollector();
 	public static OI oi;
@@ -36,11 +32,6 @@ public class Robot extends IterativeRobot {
      */
     public void robotInit() {
 		oi = new OI();
-        chooser = new SendableChooser();
-        chooser.addDefault("Default Auto", new ExampleCommand());
-//        chooser.addObject("My Auto", new MyAutoCommand());
-        SmartDashboard.putData("Auto mode", chooser);
-        System.out.println("Hello World");
     }
 	
 	/**
