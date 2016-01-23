e72944b29c9a27134d9eeb8fa64b2b14b1866009
Cross Cordosi <ccordosi1@icloud.com>
2016-01-23 12:02:05 -0500

    Added motor controllers to drivetrain clas

--- src/org/usfirst/frc/team5590/robot/subsystems/Drivetrain.java
+++ src/org/usfirst/frc/team5590/robot/subsystems/Drivetrain.java
@@ -2,21 +2,40 @@ package org.usfirst.frc.team5590.robot.subsystems;
 
 import edu.wpi.first.wpilibj.*;
 import edu.wpi.first.wpilibj.command.Subsystem;
-import org.usfirst.frc.team5590.robot.RobotMap;
+import edu.wpi.first.wpilibj.livewindow.LiveWindow;
+import edu.wpi.first.wpilibj.livewindow.LiveWindowSendable;
+
 import org.usfirst.frc.team5590.robot.commands.StopDriving;
 
-/**
- *
- */
-
 public class Drivetrain extends Subsystem {
-    
-	SpeedController leftTrack = RobotMap.drivetrainLeftTrackMotor;
-	SpeedController rightTrack = RobotMap.drivetrainRightTrackMotor;
-	RobotDrive robotDrive = RobotMap.robotDrive;
+	
+	private static int leftTrackSlot = 1;
+    private static int rightTrackSlot = 2;
+	private static RobotDrive robotDrive;
+	private static SpeedController leftTrackController;
+    private static SpeedController rightTrackController;
 	
 	// Put methods for controlling this subsystem
     // here. Call these from Commands.
+	
+	/**
+	 * Initializes Talon Speed Controllers without needing an existing instance.
+	 */
+	public static void initializeControllers(){
+		
+		leftTrackController = new TalonSRX(leftTrackSlot);
+    	LiveWindow.addActuator("Drivetrain", "Left Track", (LiveWindowSendable) leftTrackController);
+    	
+    	rightTrackController = new TalonSRX(rightTrackSlot);
+    	LiveWindow.addActuator("Drivetrain", "Left Track", (LiveWindowSendable) rightTrackController);
+    
+    	robotDrive = new RobotDrive(leftTrackController, rightTrackController);
+    	
+    	robotDrive.setSafetyEnabled(false);
+    	robotDrive.setExpiration(.1);
+    	robotDrive.setSensitivity(.5);
+    	robotDrive.setMaxOutput(1.0);
+	}
 	
 	public void takeJoystickInput(double left, double right) {
 		robotDrive.tankDrive(left, right);
