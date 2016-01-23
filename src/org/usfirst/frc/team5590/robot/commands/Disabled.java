ee04018295b241da20aee2f0ff3e3d0d5d989de8
Administrator <administrator@SJCI.COM>
2016-01-23 13:12:44 -0500

    Added breach arm substem and disabled command as default Breach Arm command.

--- src/org/usfirst/frc/team5590/robot/commands/Disabled.java
+++ src/org/usfirst/frc/team5590/robot/commands/Disabled.java
@@ -1,29 +1,23 @@
 package org.usfirst.frc.team5590.robot.commands;
 
-import org.usfirst.frc.team5590.robot.Robot;
-import org.usfirst.frc.team5590.robot.subsystems.Drivetrain;
-
 import edu.wpi.first.wpilibj.command.Command;
 
-/** This command sets the speed of both speed controllers to 0.6
- * To make make both tracks drive the robot forward
+/**
+ *
  */
-public class Drive extends Command {
-	
-	private Drivetrain drivetrain = Robot.drivetrain;
+public class Disabled extends Command {
 
-    public Drive() {
+    public Disabled() {
+        // Use requires() here to declare subsystem dependencies
+        // eg. requires(chassis);
     }
 
     // Called just before this Command runs the first time
     protected void initialize() {
-    	System.out.println("Initializing Drive Command");
-    	drivetrain.stop();
     }
 
     // Called repeatedly when this Command is scheduled to run
     protected void execute() {
-    	drivetrain.updateSpeed();
     }
 
     // Make this return true when this Command no longer needs to run execute()
@@ -33,7 +27,6 @@ public class Drive extends Command {
 
     // Called once after isFinished returns true
     protected void end() {
-    	Robot.drivetrain.stop();
     }
 
     // Called when another command which requires one or more of the same
