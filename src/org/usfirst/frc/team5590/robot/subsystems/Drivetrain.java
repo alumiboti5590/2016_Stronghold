2c3a252cca7f008c385c40fc316e3ef7748dd074
Administrator <administrator@SJCI.COM>
2016-01-23 12:03:16 -0500

    not sure what I did

--- src/org/usfirst/frc/team5590/robot/subsystems/Drivetrain.java
+++ src/org/usfirst/frc/team5590/robot/subsystems/Drivetrain.java
@@ -3,12 +3,7 @@ package org.usfirst.frc.team5590.robot.subsystems;
 import edu.wpi.first.wpilibj.*;
 import edu.wpi.first.wpilibj.command.Subsystem;
 import org.usfirst.frc.team5590.robot.RobotMap;
-import org.usfirst.frc.team5590.robot.commands.StopDriving;
 import org.usfirst.frc.team5590.robot.commands.Drive;
-
-/**
- *
- */
 
 public class Drivetrain extends Subsystem {
     
