b69a739e27c1603c5be7589c96017d0857e751b4
Dan Starner <starner.daniel5@gmail.com>
2016-02-15 11:52:56 -0500

    Collector Switch Stop

--- src/org/usfirst/frc/team5590/robot/commands/Collect.java
+++ src/org/usfirst/frc/team5590/robot/commands/Collect.java
@@ -23,12 +23,13 @@ public class Collect extends Command {
     }
 
     protected void execute() {
-    	Robot.collector.setCollectorSpeed(1);
+    		Robot.collector.setCollectorSpeed(1);
     }
 
     // Make this return true when this Command no longer needs to run execute()
     protected boolean isFinished() {
-    	return !this.button.get();
+    	// If either switch was hit after command started or button lifted
+    	return (timeSinceInitialized() > .15 && Robot.collector.getSwitchVoltage()) || !this.button.get();
     }
 
     // Called once after isFinished returns true
