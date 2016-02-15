c8b5c88d39cc471f389e27fa602dfbe09d70e536
Dan Starner <starner.daniel5@gmail.com>
2016-02-15 12:14:30 -0500

    Shooter (Hopefully) setup

--- src/org/usfirst/frc/team5590/robot/commands/Collect.java
+++ src/org/usfirst/frc/team5590/robot/commands/Collect.java
@@ -9,11 +9,12 @@ import edu.wpi.first.wpilibj.command.Command;
  *
  */
 public class Collect extends Command {
-	
+
 	Button button;
 	
     public Collect(Button button) {
     	requires(Robot.collector);
+    	requires(Robot.shooter);
     	this.button = button;
     }
 
@@ -36,6 +37,12 @@ public class Collect extends Command {
     protected void end() {
     	System.out.println("collector control ended");
     	Robot.collector.stopCollector();
+    	double startTime = timeSinceInitialized();
+    	// Slight set back to make sure not in shooter
+    	while(timeSinceInitialized() - startTime < .05) {
+    		Robot.shooter.setShooterSpeed(-.1);
+    	}
+    	Robot.shooter.stopShooter();
     }
 
     // Called when another command which requires one or more of the same
--- src/org/usfirst/frc/team5590/robot/commands/Shoot.java
+++ src/org/usfirst/frc/team5590/robot/commands/Shoot.java
@@ -11,6 +11,7 @@ import edu.wpi.first.wpilibj.command.Command;
 public class Shoot extends Command {
 
 	private Button button;
+	private boolean isComplete = false;
 	
     public Shoot(Button button) {
         requires(Robot.shooter);
@@ -25,18 +26,22 @@ public class Shoot extends Command {
 
     // Called repeatedly when this Command is scheduled to run
     protected void execute() {
-    	Robot.collector.setCollectorSpeed(0.5);
-    	Robot.shooter.setShooterSpeed(1);
+    	while (timeSinceInitialized() < 3.0) {
+    		Robot.shooter.setShooterSpeed(1);
+    	}
+    	Robot.collector.setCollectorSpeed(1);
+    	isComplete = true;
     }
 
     // Make this return true when this Command no longer needs to run execute()
     protected boolean isFinished() {
-    	return !this.button.get();
+    	return !this.button.get() || isComplete;
     }
 
     // Called once after isFinished returns true
     protected void end() {
     	System.out.println("Stopping shooter");
+    	Robot.collector.stopCollector();
     	Robot.shooter.stopShooter();
     }
 
